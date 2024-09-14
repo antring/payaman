dc2c075687bb3c1b01e317e63ef072e992b7872e
Erik <emauring@gmail.com>
2024-09-14 23:15:29 +0200

    Added regex for pacman and yay

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
+use regex::{Captures, Regex};
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, Result};
 use std::path::Path;
-use regex::Regex;
 
 fn main() {
     if let Ok(lines) = read_lines("/home/erik/.bash_history") {
         for line in lines {
-            println!("{}", line.unwrap());
+            let line_result = get_package_from_line(line.unwrap());
+
+            if let Ok(packages) = line_result {
+                println!("Package: {}", packages.join(""));
+            }
         }
-    } else { println!("No history found"); }
+    } else {
+        println!("No history found");
+    }
 }
 
-fn get_packages(line: String) -> Result<Vec<String>> {
-    let re = Regex::new(r"^pacman -S (.+)$").unwrap();
-    let mut packages = Vec::new();
-    let Some(captures) = re.captures(&*line);
+fn get_package_from_line(line: String) -> Result<Vec<String>> {
+    let re = Regex::new(r"^(sudo\s+)?(pacman|yay)\s+-S (?<packages>.+)$").unwrap();
+    let mut packages: Vec<String> = Vec::new();
+
+    let Some(captures): Option<Captures> = re.captures(&line) else {
+        return Err(io::Error::new(io::ErrorKind::Other, "Invalid line"));
+    };
+
+    packages.push(captures["packages"].parse().unwrap());
 
     Ok(packages)
 }
